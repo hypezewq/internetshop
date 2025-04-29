d380a676945b50432ac554fd3caccc9b49bc884a
hypezewq <quoiaaz@gmail.com>
2025-04-29 20:55:54 +0500

    чутка апйдет

---
+++ app/models.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "Users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    email: Mapped[str] = mapped_column(String(120), index=True)
+    password: Mapped[Optional[str]] = mapped_column(String(256))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
+
